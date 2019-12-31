e8df843f9b27e0070e43cc7b3d1352d387935959
Casey Backes <casey@Caseys-MacBook-Pro.local>
2019-12-30 21:17:25 -0700

    included function in s3_data_transfer.py to handle single file uploads for geometry shape files; modified s3 bucket in aws

--- src/s3_data_transfer.py
+++ src/s3_data_transfer.py
@@ -18,20 +18,29 @@ def print_s3_contents_boto3(connection, just_bucket_name = False):
             for key in bucket.objects.all():
                 print('\t|___', key.key)
 
-def s3_upload(bucketname, file_list):
+def s3_bulk_upload(bucketname, file_list):
     # write results back to s3
     count = 1
     s3_client = boto3.client('s3')
     for file in file_list:
-        file = 'data/'+file
+        file = '../data/'+file
         with open(file, 'rb') as data:
             
             #remove data directory from filename
-            file_basename = file.split('data/')[1]
+            file_basename = os.path.basename(file)
             fname = file_basename.split('.csv')[0]+'.csv'
             #print(f'uploading {file} as {fname}' )
-            #s3_client = boto3.client('s3')
             # s3_client.upload_file('file-on-my-local-machine.txt', bucket_name, 'upload-to-s3-as-this-filename.txt')
-            s3_client.upload_file(file, bucketname,fname  )
+            s3_client.upload_file(file, bucketname+str('/capitalbikeshare_tripdata/'),fname  )
             count+=1
         
+def s3_upload(bucketname, filepath, folder=None, save_as = None):
+    s3_client = boto3.client('s3')
+    # if save_as:
+    #     fname = save_as
+    # else:
+    #     fname = os.path.basename(filepath)
+    fname = (save_as if save_as else os.path.basename(filepath))
+    # s3_client.upload_file('file-on-my-local-machine.txt', specific_bucket_name, 'upload-to-s3-as-this-filename.txt')
+    s3_client.upload_file(filepath, bucketname,f'{str(folder+str("/")) if folder else ""}{fname}')
+    print(f'Successfully uploaded {fname} to bucket <{bucketname}>')
